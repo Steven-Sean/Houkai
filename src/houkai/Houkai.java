28d9133066c9012576ec96fd3418b882b51e7f13
Steven Sean <133088404+Steven-Sean@users.noreply.github.com>
2023-05-25 17:36:49 +0700

    Add basic PApplet code

--- src/houkai/Houkai.java
+++ src/houkai/Houkai.java
@@ -4,17 +4,39 @@
  */
 package houkai;
 
+import java.io.InputStream;
+import processing.core.*;
+
 /**
  *
  * @author Steven
  */
-public class Houkai {
+public class Houkai extends PApplet {
+    
+    // resolution
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
 
+    // sprite pixel size
+    public static final int SPRITESIZE = 20;
+    
+    // Health bar
+    public static final int TOPBAR = 80;
+    
+    // FPS Values
+    public static final int FPS = 60;
+    
+    // Assets
+    public PImage bg;
+    public PImage title;
+
+    public InputStream in;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PApplet.main("houkai.Houkai");
     }
     
 }
