917d80a2bd79c25e25f8e229be6717a9cdbe3757
Steven Sean <133088404+Steven-Sean@users.noreply.github.com>
2023-05-25 17:59:54 +0700

    Apply code formatting

--- src/houkai/Houkai.java
+++ src/houkai/Houkai.java
@@ -12,39 +12,39 @@ import processing.core.*;
  * @author Steven
  */
 public class Houkai extends PApplet {
-    
+
     // resolution
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
 
     // sprite pixel size
     private static final int SPRITE_SIZE = 20;
-    
+
     // Health bar
     private static final int TOPBAR = 80;
-    
+
     // FPS Values
     private static final int FPS = 60;
-    
+
     // Assets
     private PImage bg;
     private PImage title;
 
     private InputStream in;
-    
+
     public void settings() {
-        size(WIDTH,HEIGHT);
+        size(WIDTH, HEIGHT);
     }
-    
+
     public void setup() {
         frameRate(FPS);
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         PApplet.main("houkai.Houkai");
     }
-    
+
 }
